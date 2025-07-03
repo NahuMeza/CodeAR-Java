b0984e15269d8f86f3fd3ce8fb431965db42367b
NahuMeza <nahuelgmeza@gmail.com>
2025-07-03 19:09:43 -0300

    first commit

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo {
+    public static void main(String[] args){
+        // Int, String, Float, Bool
+        int num = 12;
+        System.out.println(num);
+    }
+}
+
+// fetch("web")
