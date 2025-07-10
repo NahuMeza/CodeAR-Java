f1519995dbb9cca9c7a7ac82016dd1f8d49fd92b
NahuMeza <nahuelgmeza@gmail.com>
2025-07-10 16:15:13 -0300

    fix: Paths

---
+++ Clase 2/EjemplosTipos.java
@@ -0,0 +1,21 @@
+public class EjemplosTipos {
+    public static void main(String args[]){
+
+        // Tipos enteros
+        int entero = 12;
+        byte bits8 = 1;
+        long enteroGrande = 100000L;
+        // Tipos decimales
+        float pi = 3.14f;
+        double piMas = 3.14159;
+        // Caracteres
+        char caracter = 'a';
+        String palabra = "Hola Mundo";
+        // Booleanos
+        boolean esVerdadero = true;
+
+        System.out.println(entero + ", " + bits8 + ", " + 
+                           enteroGrande + ", " + pi + ", " + piMas + ", " + caracter + ", " + 
+                           palabra + ", " + esVerdadero);
+    }
+}
